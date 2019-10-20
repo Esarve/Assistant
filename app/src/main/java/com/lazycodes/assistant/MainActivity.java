3e127cffa757ced86ef13c2667ad4ba775dbe7a1
AmitKundu <kundu.amit517@gmail.com>
2019-10-20 20:23:15 +0600

    Alert Dialog added

--- app/src/main/java/com/lazycodes/assistant/MainActivity.java
+++ app/src/main/java/com/lazycodes/assistant/MainActivity.java
@@ -3,16 +3,25 @@ package com.lazycodes.assistant;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import edu.cmu.pocketsphinx.Assets;
 import edu.cmu.pocketsphinx.Hypothesis;
@@ -23,11 +32,19 @@ import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 import java.io.File;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends Activity implements RecognitionListener {
+public class MainActivity extends AppCompatActivity implements RecognitionListener {
 
     Button btnRec;
     TextView dispTxt;
+    private ToggleButton saveTggBtn;
+
+    private boolean boolSaveMode; // Check if save mood is on or not
+    int pinNumber;
+    boolean nothingSelect; // Determine if anything is selected on the Spinner or not
+
     String str;
     private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
     private SpeechRecognizer recognizer;
@@ -36,8 +53,13 @@ public class MainActivity extends Activity implements RecognitionListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //main
         btnRec = findViewById(R.id.btnRec);
         dispTxt = findViewById(R.id.dsplyRslt);
+        saveTggBtn = findViewById(R.id.saveToggleBtn);
+
+
         new SetupSphinx(this).execute();
         btnRec.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -50,12 +72,110 @@ public class MainActivity extends Activity implements RecognitionListener {
                         break;
                     case MotionEvent.ACTION_UP:
                         recognizer.stop();
+                        if(boolSaveMode)
+                        {
+/*
+                            View v = getLayoutInflater().inflate(R.layout.save_pop_up_resource, null);
+
+                            cancelPopup = v.findViewById(R.id.cancelPopUpBtn);
+                            saveResultDialog.setContentView(R.layout.save_pop_up_resource);
+                            saveResultDialog.show();
+                            cancelPopup.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    saveResultDialog.dismiss();
+                                }
+                            });
+                            Spinner spinner = v.findViewById(R.id.pinSpinner);
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, R.layout.support_simple_spinner_dropdown_item,
+                                    getResources().getStringArray(R.array.pinNo) );
+
+                            spinner.setAdapter(adapter);*/
+
+                            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                            View v = getLayoutInflater().inflate(R.layout.save_pop_up_resource, null);
+
+                            Spinner spinner = v.findViewById(R.id.pinSpinner);
+                            Button cancelPopup = v.findViewById(R.id.cancelPopUpBtn);
+                            Button savepopUp = v.findViewById(R.id.savePopUpBtn);
+                            TextView textViewPopup = v.findViewById(R.id.popUpTV);
+
+
+                            alert.setView(v);
+                            final AlertDialog alertDialog = alert.create();
+                            alertDialog.setCanceledOnTouchOutside(false);
+
+                            textViewPopup.setText(str);
+
+
+                           //All spinner jobs
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
+                                    android.R.layout.simple_spinner_dropdown_item,
+                                    getPinNumber());
+                            spinner.setAdapter(adapter);
+
+                            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                                    if (getPinNumber().get(i)=="Choose One"){
+                                        nothingSelect =  true;
+                                    }
+
+                                    else {
+                                        pinNumber = Integer.parseInt(getPinNumber().get(i));
+                                        Log.d("PIN", "onItemSelected: "+pinNumber);
+                                        nothingSelect = false;
+                                    }
+
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> adapterView) {
+                                    nothingSelect =  true;
+                                }
+                            });
+
+
+
+                            //Save and cancel Button
+                            cancelPopup.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    alertDialog.dismiss();
+                                }
+                            });
+
+                            savepopUp.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    if(!nothingSelect){
+                                        // We will save the pin
+                                    }
+                                }
+                            });
+                            alertDialog.show();
+
+                        }
+
                         break;
 
                 }
                 return false;
             }
         });
+
+        saveTggBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                   boolSaveMode = true;
+
+                } else {
+                   boolSaveMode =  false;
+                }
+            }
+        });
+
     }
 
     @Override
@@ -165,4 +285,29 @@ public class MainActivity extends Activity implements RecognitionListener {
             recognizer.shutdown();
         }
     }
+
+    private List<String> getPinNumber()
+    {
+        ArrayList<String> pinNumber = new ArrayList<>();
+
+        pinNumber.add("Choose One");
+        pinNumber.add("0");
+        pinNumber.add("1");
+        pinNumber.add("2");
+        pinNumber.add("3");
+        pinNumber.add("4");
+        pinNumber.add("5");
+        pinNumber.add("6");
+        pinNumber.add("7");
+        pinNumber.add("8");
+        pinNumber.add("9");
+        pinNumber.add("10");
+        pinNumber.add("11");
+        pinNumber.add("12");
+        pinNumber.add("13");
+
+        return pinNumber;
+
+    }
+
 }
